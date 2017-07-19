435f561de6120d0a42805ae69ea8a9e6f240b670
LAU Thierry <thierry.lau@perfectstay.com>
2017-07-20 01:29:55 +0200

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppContainer from './AppContainer';
-import ChildPage from "./ChildPage";
-import ChildrenListContainer from "./ChildrenListContainer";
 import store from "./store";
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from "react-redux";
