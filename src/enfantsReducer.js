c605e73714dde597d317d82b3ae53aea758f4ebe
LAU Thierry <thierry.lau@perfectstay.com>
2017-07-20 21:08:10 +0200

    posibilite de mettre un enfant en absence pour une journee

--- src/enfantsReducer.js
+++ src/enfantsReducer.js
@@ -12,23 +12,33 @@ export default (enfants = {bebes: [], moyens: []}, action) => {
 					planning: {
 						lundi: {
 							debut: bebe.lundi_arrivee,
-							fin: bebe.lundi_depart
+							fin: bebe.lundi_depart,
+							present: bebe.present_lundi
+
 						},
 						mardi: {
 							debut: bebe.mardi_arrivee,
-							fin: bebe.mardi_depart
+							fin: bebe.mardi_depart,
+							present: bebe.present_mardi
+
 						},
 						mercredi: {
 							debut: bebe.mercredi_arrivee,
-							fin: bebe.mercredi_depart
+							fin: bebe.mercredi_depart,
+							present: bebe.present_mercredi
+
 						},
 						jeudi: {
 							debut: bebe.jeudi_arrivee,
-							fin: bebe.jeudi_depart
+							fin: bebe.jeudi_depart,
+							present: bebe.present_jeudi
+
 						},
 						vendredi: {
 							debut: bebe.vendredi_arrivee,
-							fin: bebe.vendredi_depart
+							fin: bebe.vendredi_depart,
+							present: bebe.present_vendredi
+
 						}
 					}
 				}
@@ -45,23 +55,28 @@ export default (enfants = {bebes: [], moyens: []}, action) => {
 					planning: {
 						lundi: {
 							debut: moyen.lundi_arrivee,
-							fin: moyen.lundi_depart
+							fin: moyen.lundi_depart,
+							present: moyen.present_lundi
 						},
 						mardi: {
 							debut: moyen.mardi_arrivee,
-							fin: moyen.mardi_depart
+							fin: moyen.mardi_depart,
+							present: moyen.present_mardi
 						},
 						mercredi: {
 							debut: moyen.mercredi_arrivee,
-							fin: moyen.mercredi_depart
+							fin: moyen.mercredi_depart,
+							present: moyen.present_mercredi
 						},
 						jeudi: {
 							debut: moyen.jeudi_arrivee,
-							fin: moyen.jeudi_depart
+							fin: moyen.jeudi_depart,
+							present: moyen.present_jeudi
 						},
 						vendredi: {
 							debut: moyen.vendredi_arrivee,
-							fin: moyen.vendredi_depart
+							fin: moyen.vendredi_depart,
+							present: moyen.present_vendredi
 						}
 					}
 				}
